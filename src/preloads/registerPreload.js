34ea5e7a690ae0d19e686ab97865f0716a535416
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-12-18 20:47:27 -0300

    estilos a registro y login

--- src/preloads/registerPreload.js
+++ src/preloads/registerPreload.js
@@ -1,31 +1,26 @@
 const { ipcRenderer } = require('electron')
 
-let nombre; 
-let apellido; 
-let email; 
-let password; 
+let nombre;
+let apellido;
+let email;
+let password;
 let regbtn;
 let volver;
-let lider;
 
-window.onload = function() { 
+window.onload = function () {
   email = document.getElementById("reg_email")
   password = document.getElementById("reg_pass")
   nombre = document.getElementById("reg_user")
   apellido = document.getElementById("reg_apellido")
-  lider = document.getElementById("reg_lider")
   regbtn = document.getElementById("reg_btn")
   volver = document.getElementById("volver_login")
 
-  regbtn.onclick = function(){
-    if(nombre.value != null && nombre.value != "", apellido.value != null && apellido.value != "", email.value != null && email.value != "", password.value != null && password.value != ""){
-      const obj = {nombre: nombre.value, apellido:apellido.value, email:email.value, password:password.value, lider:lider.value}
-      ipcRenderer.invoke("register", obj) 
-    }
-       
+  regbtn.onclick = function () {
+    const obj = { nombre: nombre.value, apellido: apellido.value, email: email.value, password: password.value }
+    ipcRenderer.invoke("register", obj)
   }
 
-  volver.onclick = function(){
+  volver.onclick = function () {
     ipcRenderer.invoke("moveToLogin")
   }
 }
